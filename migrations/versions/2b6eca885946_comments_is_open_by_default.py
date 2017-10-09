f5ac805cf2809baa8694d21c0b138830de667958
KenOokamiHoro <horo@yoitsu.moe>
2017-10-09 21:42:54 +0800

    update database

---
+++ migrations/versions/2b6eca885946_comments_is_open_by_default.py
@@ -0,0 +1,24 @@
+"""comments is open by default
+
+Revision ID: 2b6eca885946
+Revises: d0535a3adec1
+Create Date: 2017-10-09 21:40:36.146216
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '2b6eca885946'
+down_revision = 'd0535a3adec1'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
